ec817d21f243676b8ddddace635d2bdff99b7f55
Shreya Naik <16co37@aitdgoa.edu.in>
2019-07-22 16:51:20 +0530

    final prog

--- exp1/ex1.cpp
+++ exp1/ex1.cpp
@@ -1,36 +1,38 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
-int isnumeric(char);
+
 int main()
 {
-	char array[10];
+	
+	string str,arr[10];
 	int n;
-	cout<<"\nEnter the size";
+	cout<<"\nEnter the number of terms: ";
 	cin>>n;
-	cout<<"\nEnter the data:";
+	cout<<"\nEnter each term:";
 	for(int i =0; i<n ; i++)
 	{
-		cin>>array[i];
+		cin>>str;
+		arr[i]=str;
 	}
 	for(int i=0; i<n ; i++)
 	{
-		char val=array[i];
-		if(isnumeric(val))
-		{
-			cout<<"\n"<<val<<" is a number";
-		}
-		else
-		{
-			cout<<"\n"<<val<<" is a character";
-		}
-	}	
+		   if(arr[i]=="+"||arr[i]=="-" || arr[i]=="<" || arr[i]==">" ||arr[i]=="*")
+		   cout<<"\n"<<arr[i]<<" is a operator";
+		   else if(arr[i]==";" || arr[i]=="," || arr[i]=="(" || arr[i]==")")
+		   cout<<"\n"<<arr[i]<<" is a delimiter";
+		   else if(arr[i]=="for" || arr[i]=="if" || arr[i]=="int" || arr[i]=="float" || arr[i]=="while" ||arr[i]=="string")
+		   cout<<"\n"<<arr[i]<<" is a keyword";
+		   else
+		   cout<<"\n"<<arr[i]<<" is a identifier";
+	   }
+   return 0;
 }
- int isnumeric(char a)
- {
-	 if(a=='0' || a=='1' || a=='2' || a=='3' || a=='4' || a=='5' || a=='6' || a=='7' || a=='8' || a=='9')
-	 return 1;
-	 else
-	 return 0;
- }
+ 
+
+ 
+   
+   
+		
 
 	 
